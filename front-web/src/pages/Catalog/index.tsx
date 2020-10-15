55e39e1504ecd7d9db261d07ca3b3ea5e4404bdb
RanyellHenrique <ranyellhenrique@gmail.com>
2020-10-15 13:51:44 -0300

    Add Admin navbar

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -5,23 +5,25 @@ import { Link } from 'react-router-dom';
 import { makeRequest } from 'core/utils/request';
 import { ProductsResponse } from 'core/types/Products';
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
+import Pagination from 'core/components/Pagination';
 
 
 const Catalog = () => {
 
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
     const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
 
     useEffect(() => {
         const params = {
-            page: 0,
+            page: activePage,
             linesPerPage: 12
         }
         setIsLoading(true);
         makeRequest({ url: '/products', params })
             .then(response => setProductsResponse(response.data))
             .finally(() => setIsLoading(false));
-    }, [])
+    }, [activePage])
 
     return (
         <div className="catalog-container">
@@ -31,9 +33,17 @@ const Catalog = () => {
             <div className="catalog-products">
                 {isLoading ? <ProductCardLoader /> :
                     productsResponse?.content.map(product => (
-                        <Link to={`/products/${product.id}`} key={product.id}><ProductCard product={product} /></Link>
+                        <Link to={`/products/${product.id}`} key={product.id}>
+                            <ProductCard product={product} />
+                        </Link>
                     ))}
             </div>
+            {productsResponse && 
+                <Pagination 
+                    totalPages={productsResponse.totalPages}
+                    activePage={activePage}
+                    onChange={page => setActivePage(page)}
+                />}
         </div>
     );
 }
