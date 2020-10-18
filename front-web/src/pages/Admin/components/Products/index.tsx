6d05a2d2d132cd90ca750f22ea7864c2c023221b
RanyellHenrique <ranyellhenrique@gmail.com>
2020-10-18 00:05:18 -0300

    Add base admin form structure

---
+++ front-web/src/pages/Admin/components/Products/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './styles.scss';
+import { Switch, Route } from 'react-router-dom';
+import List from './List';
+import Form from './Form';
+
+
+const Products = () => {
+
+    return (
+        <div>
+            <Switch>
+                <Route path="/admin/products" exact>
+                   <List />
+                </Route>
+                <Route path="/admin/products/create">
+                    <Form/>
+                </Route>
+                <Route path="/admin/products/:productId">
+                    <h1>Editar um  Produto</h1>
+                </Route>
+            </Switch>
+        </div>
+    );
+}
+
+export default Products;
