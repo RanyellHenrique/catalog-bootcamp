0603251d24436e34b8ae173c379c010ea12ad19b
RanyellHenrique <ranyellhenrique@gmail.com>
2020-11-13 01:32:17 -0300

    Apply login form validations

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './styles.scss';
 import BaseForm from '../../BaseForm';
-import { makeRequest } from 'core/utils/request';
+import { makePrivateRequest } from 'core/utils/request';
 
 type FormState = {
     name: string;
@@ -30,7 +30,7 @@ const Form = () => {
             imgUrl: 'https://cdn.awsli.com.br/300x300/1147/1147622/produto/71051088/3f1c1728dd.jpg',
             categories: [{ id: formData.category }]
         }
-        makeRequest({ url: '/products', method: 'POST', data: payload })
+        makePrivateRequest({ url: '/products', method: 'POST', data: payload })
             .then(() => {
                 setFormData({name:'', category:'1', price:'', description: ''});
             });
--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,42 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.scss';
 import AuthCard from '../Card';
-import { Link } from 'react-router-dom';
-import {useForm} from 'react-hook-form';
+import { Link, useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import ButtonIcon from 'core/components/ButtonIcon';
+import { makeLogin } from 'core/utils/request';
+import { saveSessionData } from 'core/utils/auth';
 
 type FormData = {
-    email: string;
+    username: string;
     password: string;
 }
 
 
 const Login = () => {
 
-    const {register, handleSubmit} = useForm<FormData>();
+    const { register, handleSubmit, errors } = useForm<FormData>();
+    const [hasError, setHasError] = useState(false);
+    const history = useHistory();
 
     const onSubmit = (data: FormData) => {
         console.log(data);
+        makeLogin(data)
+            .then(response => {
+                setHasError(false);
+                saveSessionData(response.data);
+                history.push('/admin');
+            })
+            .catch(() => {
+                setHasError(true);
+            })
     }
 
     return (
         <AuthCard title="login">
+            {hasError && (
+                <div className="alert alert-danger mt-5">
+                    Usuário ou senha inválidos!
+                </div>
+            )}
             <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-                <input
-                    type="email"
-                    className="form-control input-base margin-bottom-30"
-                    placeholder="Email"
-                    name="email"
-                    ref={register}
-                />
+                <div className="margin-bottom-30">
+                    <input
+                        type="email"
+                        className={`form-control input-base ${errors.username ? 'is-invalid': ''}`}
+                        placeholder="Email"
+                        name="username"
+                        ref={register({
+                            required: "Campo obrigatório",
+                            pattern: {
+                              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                              message: "Email inválido"
+                            }
+                          })}
+                    />
+                    {errors.username && (
+                        <div className="invalid-feedback d-block">
+                            {errors.username.message}
+                        </div>
+                    )}
+                </div>
 
-                <input
-                    type="password"
-                    className="form-control input-base"
-                    placeholder="Senha"
-                    name="password"
-                    ref={register}
-                />
+                <div className="margin-bottom-30">
+                    <input
+                        type="password"
+                        className={`form-control input-base ${errors.password ? 'is-invalid': ''}`}
+                        placeholder="Senha"
+                        name="password"
+                        ref={register({ required: "Campo obrigatório" })}
+                    />
+                    {errors.password && (
+                        <div className="invalid-feedback d-block">
+                            {errors.password.message}
+                        </div>
+                    )}
+                </div>
+
                 <Link to="/admin/auth/recover" className="login-link-recover">
                     Esqueci a senha ?
                 </Link>
