6c071d2d217ee57047e29c070c187995c319639d
RanyellHenrique <ranyellhenrique@gmail.com>
2020-12-15 17:01:28 -0300

    Add productCard and searchInput

--- mobile/src/pages/Catalog.tsx
+++ mobile/src/pages/Catalog.tsx
@@ -1,13 +1,59 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import { ProductCard, SearchInput } from '../components';
+import productImg from '../assets/product.png';
+import { theme } from '../styles';
+
+const products = [
+    {
+        id: 1,
+        imgUrl: productImg,
+        name: "Smartphone",
+        price: 2279.0
+    },
+    {
+        id: 2,
+        imgUrl: productImg,
+        name: "Computador Desktop - Intel Core i7",
+        price: 2279.0
+    },
+    {
+        id: 3,
+        imgUrl: productImg,
+        name: "Computador Desktop - Intel Core i7",
+        price: 2279.0
+    },
+    {
+        id: 4,
+        imgUrl: productImg,
+        name: "Computador Desktop - Intel Core i7",
+        price: 2279.0
+    },
+    {
+        id: 5,
+        imgUrl: productImg,
+        name: "Computador Desktop - Intel Core i7",
+        price: 2279.0
+    }
+]
 
 const Catalog: React.FC = () => {
+
+    const [search, setSearch] = useState("");
+
+    const data = search.length > 0 
+        ? products.filter(product => product.name.toLowerCase().includes(search.toLowerCase())) 
+        : products;
+
     return (
-        <View>
-            <Text>
-                Catalog
-            </Text>
-        </View>
+        <ScrollView contentContainerStyle={theme.scrollContainer}>
+            <SearchInput
+                placeholder="Nome do Produto"
+                search={search}
+                setSearch={setSearch}
+            />
+            {data.map(product => <ProductCard {...product} key={product.id} />)}
+        </ScrollView>
     )
 }
 
--- mobile/src/styles/index.ts
+++ mobile/src/styles/index.ts
@@ -1,4 +1,5 @@
 import {StyleSheet} from 'react-native';
+import { color } from 'react-native-reanimated';
 
 const colors = {
     white: "#FFFFFF",
@@ -9,7 +10,8 @@ const colors = {
     primary: "#407BEE",
     secondary: "#33569B",
     bluePill: "#407BFF61",
-    red: "#DF5753"
+    red: "#DF5753",
+    borderGray: "#E1E1E1"
 }
 
 const theme = StyleSheet.create({
@@ -58,6 +60,60 @@ const theme = StyleSheet.create({
         borderTopRightRadius: 10,
         alignItems: "center",
         justifyContent: "center"
+    },
+    //productCard
+    productCard: {
+        width: "100%",
+        backgroundColor: colors.white,
+        borderRadius: 10,
+        shadowColor: colors.black,
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        marginVertical: 10,
+        alignItems: "center",
+        justifyContent: "space-around"
+
+    },
+    productDescription: {
+        width: "100%",
+        padding: 20,
+        borderTopColor: colors.lightGray,
+        borderTopWidth: 1
+    },
+    priceContainer: {
+        flexDirection: "row",
+        marginTop: 10,
+    },
+    scrollContainer: {
+        padding: 10
+    },
+    //Search Input
+    inputContainer: {
+        width: "100%",
+        height: 60,
+        backgroundColor: colors.white,
+        borderRadius: 10,
+        shadowColor: colors.black,
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        marginVertical: 12.5,
+        alignItems: "center",
+        paddingVertical: 10
+    },
+    searchInput: {
+        width: "90%",
+        height: 40,
+        borderBottomWidth: 0.5,
+        borderBottomColor: colors.borderGray
+
     }
 })
 
@@ -81,6 +137,20 @@ const text = StyleSheet.create({
         textTransform: "uppercase",
         color: colors.white,
         marginLeft: 20
+    },
+    productName: {
+        fontSize: 16,
+        fontWeight: "bold"
+    },
+    currency: {
+        fontSize: 16,
+        fontWeight: "400",
+        color: colors.mediumGray
+    },
+    productPrice: {
+        fontSize: 30,
+        color: colors.primary,
+        fontWeight: "bold"
     }
 })
 
